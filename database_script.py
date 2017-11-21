254ca8ee88ca4aef85891b6d825c50d9ccc66b05
Callum Court <32265608+cjcourt@users.noreply.github.com>
2017-11-21 11:17:03 +0000

    Delete database_script.py

--- database_script.py
+++
@@ -1,7 +0,0 @@
-from database import MagnetismDatabase
-
-corpus = '//home/cdt1606/Desktop/Work/Data/XML/curie/'
-corpus2 = '//home/cdt1606/Desktop/Work/Data/XML/neel/'
-mdb = MagnetismDatabase(corpus, 'phase_transitions', start=4920)
-
-mdb2 = MagnetismDatabase(corpus2, 'phase_transitions', start=0)
